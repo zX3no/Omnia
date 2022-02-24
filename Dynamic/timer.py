3eac601544bffec7a72454dae47d8e753bad7528
zX3no <dr_xeno@hotmail.com.au>
2022-02-24 23:06:34 +1030

    simplier python

--- Dynamic/timer.py
+++ Dynamic/timer.py
@@ -1,16 +1,10 @@
 import time
   
-def countdown(t):
-    
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-      
-    print('Countdown complete!')
-  
-t = input("Enter the time in seconds: ")
-  
-countdown(int(t))
+t = int(input("Enter the time in seconds: "))
+
+while t:
+    mins, secs = divmod(t, 60)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    print(timer, end="\r")
+    time.sleep(1)
+    t -= 1
